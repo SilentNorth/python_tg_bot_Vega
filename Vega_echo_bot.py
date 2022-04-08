9f2ea793421a206c6efd5bd5e865ecb7a949b0d3
Silent_North <smolyanov.iv@gmail.com>
2022-04-08 22:41:46 +0300

    init

---
+++ Vega_echo_bot.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=["dice", "animation", "text", "audio", "document", "photo", "sticker", "video", "video_note", "voice", "pinned_message", "poll_option", "poll_answer", "poll"])
+async def echo_send(message: types.Message):
+
+    await bot.copy_message(message.chat.id, message.chat.id, message.message_id)
+
+
+executor.start_polling(dp, skip_updates=True)
